d62997c97316851b7ac096ae5a8f8ee35bcfc171
tarberd <bernardo.mferrari@gmail.com>
2016-10-17 00:21:15 -0200

    cleaning stuf

--- do_the_thing.sh
+++ do_the_thing.sh
@@ -2,7 +2,6 @@ DIRECTORY="$HOME/.vim/bundle/Vundle.vim"
 
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 
-
 if [ ! -d  "$DIRECTORY" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $DIRECTORY
 fi
