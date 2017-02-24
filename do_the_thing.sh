b9a6c06597bbf51c537d7e49b171db082c27da2f
Bernardo Ferrari <bernardo.mferrari@gmail.com>
2017-02-24 15:49:16 -0300

    yey

--- do_the_thing.sh
+++ do_the_thing.sh
@@ -1,6 +1,7 @@
 DIRECTORY="$HOME/.vim/bundle/Vundle.vim"
 
-ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf $HOME/dotfiles/i3/config $HOME/.config/i3/config
+ln -sf $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 
 if [ ! -d  "$DIRECTORY" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $DIRECTORY
