e6940020004460a0fe05711aff2bb8247519dd0c
tarberd <bernardo.mferrari@gmail.com>
2017-09-14 13:33:29 -0300

    nice rice

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example &
+polybar -c $HOME/.config/polybar/config top &
 
 
 echo "Bars launched..."
