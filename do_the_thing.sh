1779f48b9abaedfd94057221a15197184cc7a08d
tarberd <bernardo.mferrari@gmail.com>
2017-09-14 01:32:47 -0300

    lotz of ztuffer

--- do_the_thing.sh
+++ do_the_thing.sh
@@ -1,9 +1,23 @@
-DIRECTORY="$HOME/.vim/bundle/Vundle.vim"
+git config --global user.email "bernardo.mferrari@gmail.com"
+git config --global user.name "tarberd"
 
-ln -sf $HOME/dotfiles/i3/config $HOME/.config/i3/config
+DIRECTORY_VUNDLE="$HOME/.vim/bundle/Vundle.vim"
+
+mkdir $HOME/.config
+mkdir $HOME/.config/i3
+mkdir $HOME/.config/sway
+mkdir $HOME/.config/termite
+
+ln -sf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/vim/vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/i3/config $HOME/.config/i3/config
+ln -sf $HOME/dotfiles/sway/config $HOME/.config/sway/config
+ln -sf $HOME/dotfiles/termite/config $HOME/.config/termite/config
 
-if [ ! -d  "$DIRECTORY" ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git $DIRECTORY
+if [ ! -d  "$DIRECTORY_VUNDLE" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $DIRECTORY
 fi
 
+if [ ! -d  "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
