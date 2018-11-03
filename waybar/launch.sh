ab1d192680e86684b4890fe8a163b4f0f4cad775
tarberd <bernardo.mferrari@gmail.com>
2018-11-03 05:09:38 +0000

    Suport new installation on desktop

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,13 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
 
 # Terminate already running bar instances
 killall -q waybar
 
-# Wait until the processes have been shut down
-while pgrep -x waybar >/dev/null; do sleep 1; done
-
 # Launch bar
-# Bar is the name set in the polybar config, so if you change it, you have to change it here too.
 waybar
 
 echo "Bars launched..."
