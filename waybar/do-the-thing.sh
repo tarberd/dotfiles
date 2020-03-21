c61f0857b9e157fcdcb6ad01eb80e95ef316d9fc
tarberd <bernardo.mferrari@gmail.com>
2020-03-21 18:03:30 -0300

    Configure fonts and keyboard layout on waybar

--- waybar/do-the-thing.sh
+++ waybar/do-the-thing.sh
@@ -5,16 +5,23 @@ set -e
 source_dir=$(dirname $(readlink -f $0))
 install_dir=$HOME/.config/waybar
 
+
 _install() {
-  echo "Linking rofi configuration files to '$install_dir'."
+  echo "Linking waybar configuration files to '$install_dir'."
+  echo "Remember to install dependencies for mediaplayer and keyboard-layout scripts."
+
   install -d $install_dir
   ln -sf $source_dir/config $install_dir/config
   ln -sf $source_dir/style.css $install_dir/style.css
+  ln -sf $source_dir/mediaplayer.py $install_dir/mediaplayer.py
+  ln -sf $source_dir/keyboard-layout.sh $install_dir/keyboard-layout.sh
 }
 
 _remove() {
   rm $install_dir/config
   rm $install_dir/style.css
+  rm $install_dir/mediaplayer.py
+  rm $install_dir/keyboard-layout.sh
 }
 
 if [ $1 == "install" ]
---
+++ waybar/keyboard-layout.sh
@@ -0,0 +1,9 @@
+swaymsg -t get_inputs | jq -r \
+    "first(.[]|select(.identifier == \"$1\" and .type == \"keyboard\")) \
+    | .xkb_active_layout_name"
+
+swaymsg -mrt subscribe '["input"]' | jq -r --unbuffered \
+    "select(.change == \"xkb_layout\")
+    | .input
+    | select(.identifier == \"$1\" and .type == \"keyboard\") \
+    | .xkb_active_layout_name"
