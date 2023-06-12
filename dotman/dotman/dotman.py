bde7c9e4d0538fa49c1831712bee1ba3e9afc4a1
Bernardo Ferrari <bernardo.ferrari@tasking.com>
2023-06-12 14:45:53 +0200

    add file_map to dotman.toml

--- dotman/dotman/dotman.py
+++ dotman/dotman/dotman.py
@@ -6,70 +6,99 @@ from pathlib import Path
 dotman_cli = typer.Typer()
 
 
+class DotmanConfig:
+    def __init__(self, dotman_config_file_directory, target_config_directory, file_map):
+        self.source_dir = dotman_config_file_directory
+        self.target_dir = target_config_directory
+        self.file_map = file_map
+
+
 def main():
     dotman_cli()
 
 
 @dotman_cli.command()
-def link(path: Path):
-    if not path.is_dir():
-        typer.echo(f"Error: {path} is not a directory!")
+def link(root_path: Path):
+    if not root_path.is_dir():
+        typer.echo(f"Error: {root_path} is not a directory!")
         return
 
-    (_, subdirs, _) = next(os.walk(path))
+    (_, root_path_subdirectories, _) = next(os.walk(root_path))
 
-    subdirs = map(lambda subdir: path.joinpath(subdir), subdirs)
+    root_path_subdirectories = map(
+        lambda subdir: root_path.joinpath(subdir), root_path_subdirectories)
 
-    to_link = []
-    for subdir in subdirs:
-        (_, _, files) = next(os.walk(subdir))
-        files = [x for x in filter(lambda file: file == "dotman.toml", files)]
-        if len(files) > 0:
-            metadata_file = files[0]
+    dotman_config_file_directories = []
+    for subdir in root_path_subdirectories:
+        (_, _, config_files) = next(os.walk(subdir))
+        config_files = [x for x in filter(
+            lambda file: file == "dotman.toml", config_files)]
+        if len(config_files) > 0:
+            metadata_file = config_files[0]
             typer.echo(f"Found {metadata_file} at {subdir}.")
-            to_link.append(Path(subdir))
+            dotman_config_file_directories.append(Path(subdir))
 
-    config_paths = []
-    for dir in to_link:
-        dotman_dir = dir.joinpath("dotman.toml")
+    dotman_configurations = []
+    for source_dir in dotman_config_file_directories:
+        dotman_dir = source_dir.joinpath("dotman.toml")
         metadata = toml.load(dotman_dir)
         entries = metadata.get('dotman')
         if entries == None:
-            typer.echo(f"Error: Missing [dotman] on {dotman_dir} be an absolute path.")
+            typer.echo(
+                f"Error: Missing [dotman] on {dotman_dir} be an absolute path.")
             return
 
         config_path = entries.get('config_path')
         if entries == None:
-            typer.echo(f"Error: Missing config_path on [dotman] on {dotman_dir} be an absolute path.")
+            typer.echo(
+                f"Error: Missing config_path on [dotman] on {dotman_dir} be an absolute path.")
             return
 
         config_path = Path(os.path.expandvars(config_path))
         if not config_path.is_absolute():
-            typer.echo(f"Error: config_path: '{config_path}' must be an absolute path.")
+            typer.echo(
+                f"Error: config_path: '{config_path}' must be an absolute path.")
             return
 
-        config_paths.append(config_path)
+        file_map = entries.get('file_map')
+        if file_map == None:
+            file_map = []
+        
+        dotman_configurations.append(DotmanConfig(source_dir, config_path, file_map))
 
-    for (dir, config_path) in zip(to_link, config_paths):
-        if not os.path.exists(config_path):
-            typer.echo(f"{dir}: Creating {config_path}.")
-            os.makedirs(config_path)
+    for dotman_config in dotman_configurations:
+        source_dir = dotman_config.source_dir
+        target_dir = dotman_config.target_dir
+        file_map = dotman_config.file_map
 
-        (_, _, files) = next(os.walk(dir))
-        files = filter(lambda file: file != "dotman.toml", files)
-        for file in files:
-            file_dir = os.path.realpath(dir.joinpath(file))
-            config_file_dir = config_path.joinpath(file)
-            if os.path.islink(config_file_dir):
-                typer.echo(f"{dir}: Removing existant link at {config_file_dir}.")
-                os.unlink(config_file_dir)
-            elif os.path.isfile(config_file_dir):
-                typer.echo(f"{dir}: Removing existant file at {config_file_dir}.")
-                os.remove(config_file_dir)
-            typer.echo(f"{dir}: Creating link: {config_file_dir} -> {file_dir}.")
-            os.symlink(file_dir, config_file_dir)
+        if not os.path.exists(target_dir):
+            typer.echo(f"{source_dir}: Creating {target_dir}.")
+            os.makedirs(target_dir)
+        
+        (_, _, config_files) = next(os.walk(source_dir))
+        config_files = filter(lambda file: file != "dotman.toml", config_files)
+        for config_file in config_files:
+            source_file_path = os.path.realpath(source_dir.joinpath(config_file))
 
+            target_file_path = target_dir.joinpath(config_file)
+            for mapping in file_map:
+                if mapping['file'] == config_file:
+                    target_file_path = target_dir.joinpath(Path(mapping['link']))
 
+            if os.path.isfile(source_file_path) or os.path.islink(source_file_path):
+                if os.path.islink(target_file_path):
+                    typer.echo(
+                        f"{source_dir}: Removing existant link at {target_file_path}.")
+                    os.unlink(target_file_path)
+                elif os.path.isfile(target_file_path):
+                    typer.echo(
+                        f"{source_dir}: Removing existant file at {target_file_path}.")
+                    os.remove(target_file_path)
+                typer.echo(
+                    f"{source_dir}: Creating link: {target_file_path} -> {source_file_path}.")
+                os.symlink(source_file_path, target_file_path)
+            else:
+                typer.echo(f"{source_file_path} does not exist")
 
 
 @dotman_cli.command()
